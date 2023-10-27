dfb704c7afee07cea68ab837ce7b218c745a48a1
jay-mango <145506724+jay-mango@users.noreply.github.com>
2023-10-27 10:25:41 -0400

    Update TRON-PROJECT-1.0.c

--- TRON-PROJECT-1.0.c
+++ TRON-PROJECT-1.0.c
@@ -3,6 +3,56 @@
 //FIGURING OUT HOW TO STAY BETWEEN COLOR LANES AND ALL THAT GOOD STUFF
 
 
+void configSensors()
+{
+	// Motor Configuration
+	// Front Left: motorA
+	// Front Right: motorB
+	// Back Left: motorC
+	// Back Right: motorD
+
+	// Sensors Configuration
+	SensorType[S1] = sensorEV3_Touch;
+	wait1Msec(50);
+	SensorType[S2] = sensorEV3_Ultrasonic;
+	wait1Msec(50);
+	SensorType[S3] = sensorEV3_Color;
+	wait1Msec(50);
+	SensorType[S4] = sensorEV3_Gyro;
+	wait1Msec(50);
+
+	// Calibrate Gyro Sensor
+	SensorMode[S4] = modeEV3Gyro_Calibration;
+	wait1Msec(50);
+	SensorMode[S4] = modeEV3Gyro_RateAndAngle;
+	wait1Msec(50);
+
+	// Calibrate Colour Sensor
+	SensorMode[S3] = modeEV3Color_Color;
+	wait1Msec(50);
+
+	// Reset Gyro at the begining
+	resetGyro(S4);
+	wait1Msec(50);
+
+	// Reset nMotorEncoder
+	nMotorEncoder[motorA] = 0;
+
+	return;
+}
+
+void driveFront(int m_pow_f)
+{
+	motor[motorA] = motor[motorB] = m_pow_f;
+	return;
+}
+
+void driveBack(int m_pow_b)
+{
+	motor[motorA] = motor[motorB] = m_pow_b;
+	return;
+}
+
 
 int drive(motorPowerBack = 0, motorPowerFront = 0)
 {
@@ -26,6 +76,8 @@ int drive(motorPowerBack = 0, motorPowerFront = 0)
 
 task main()
 {
+	configSensors();
+	
 	bool flag_border = false;
 	int flag_border_check = 0;
 
